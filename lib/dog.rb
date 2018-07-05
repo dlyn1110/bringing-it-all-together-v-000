0b9087c9c0994971784d12114da4d37dc540c0e7
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 20:31:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,9 +76,9 @@ class Dog
       LIMIT 1
     SQL
     result = DB[:conn].execute(sql, name)[0]
-     self.new_from_db(row)
+     self.new_from_db(result)
     end
-  
+
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
