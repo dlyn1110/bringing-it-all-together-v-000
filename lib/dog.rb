ef48ae44986013b9d38228972b2144e9bd4fb648
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 20:09:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,7 @@ class Dog
     dog = Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
   end
 
   def self.new_from_db
